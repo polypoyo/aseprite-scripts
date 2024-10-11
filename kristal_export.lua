8f92b1373e568e85c0ae2c7622fecdc57fce13f1
hyperboid <hyperboid@nullanoid.net>
2024-10-11 13:54:16 -0400

    Add kristal_export.lua

---
+++ kristal_export.lua
@@ -0,0 +1,51 @@
+local function export(sprite, directory)
+	for _, tag in ipairs(sprite.tags) do
+		for j=tag.fromFrame.frameNumber,tag.toFrame.frameNumber do
+			local file_name = tag.name .. "_" .. (j - tag.fromFrame.frameNumber) .. ".png"
+			local tempsprite = Sprite(sprite)
+			local frame_n = j
+			tempsprite.filename = file_name
+			repeat
+				if frame_n == 1 then
+					tempsprite:deleteFrame(2)
+				else
+					tempsprite:deleteFrame(1)
+					frame_n = frame_n - 1
+				end 
+			until #tempsprite.frames == 1
+			-- sanity check (trust me it's more annoying with out this)
+			if #tempsprite.frames ~= 1 then
+				tempsprite:close()
+				error("More than one frame left somehow?")
+			end
+			tempsprite:saveCopyAs(directory .. file_name)
+			tempsprite:close()
+		end
+	end
+end
+
+local function dirname(str)
+    return str:match("(.*[/\\])")
+end
+
+local dlg = Dialog()
+dlg:entry{ id="target_dir",
+	label="Target Directory",
+	text=dirname(app.sprite.filename)}
+
+dlg:button {
+	text="Start",
+	onclick=function()
+		export(app.sprite, dlg.data.target_dir)
+		dlg:close()
+	end
+}
+
+dlg:button {
+	text="Cancel",
+	onclick=function()
+		dlg:close()
+	end
+}
+dlg:show{ wait=false }
+
